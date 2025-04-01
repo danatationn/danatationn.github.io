c81da0eb06f8c82a11aed8a093938b8e1e7eddb3
dan <lemontasteicetea@proton.me>
2025-04-01 16:41:29 +0300

    nice hover stuff and mobile layout changes

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <script setup>
+	import {ref} from "vue";
+	const currentUsername = ref('');
+	
 	var socials = [
 		['twitter', '@danatationn', 'https://x.com/danatationn'],
-		['vndb', 'u288771', 'https://vndb.org/u288771/ulist?vnlist=1'],
+		['vndb', 'u288771', 'https://vndb.org/u288771'],
 		['github', '@danatationn', 'https://github.com/danatationn'],
 		['reddit', 'u/danatationn', 'https://reddit.com/user/danatationn'],
 		['osu', 'drunkromanian', 'https://osu.ppy.sh/users/36007819'],
@@ -11,15 +14,21 @@
 </script>
 
 <template>
-	<h1>site under construction</h1>
-
-	<ul>
-		<li v-for="social in socials"> 
-			<a :href="social[2]"> {{social[0]}} </a> 
-		</li>
-	</ul>
-
-	<h5>© copyright 2025</h5>
+	<body>
+		<h1>site under construction</h1>
+	
+		<ul>
+			<li v-for="social in socials"
+				@mouseover="currentUsername=social[1]"
+				@mouseleave="currentUsername=''"> 
+				<a :href="social[2]"> {{social[0]}} </a> 
+			</li>
+		</ul>
+		
+		<h3>{{currentUsername}}</h3>
+		
+		<h5>© copyright 2025</h5>
+	</body>
 </template>
 
 <style scoped>
